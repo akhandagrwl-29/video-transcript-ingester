423007c97ad147ac0fb538b3025f1e5031cf4a10
mukul79 <55882740+mukul79@users.noreply.github.com>
2026-01-17 13:11:30 +0530

    Update youtube_transcript.py

--- youtube_transcript.py
+++ youtube_transcript.py
@@ -1,35 +1,56 @@
-from urllib.parse import parse_qs, urlparse
-
+from pytube import Playlist
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptFound
+import os
+import ssl
+import certifi
+import time
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import WebshareProxyConfig
 
 
-def get_video_id(youtube_url: str) -> str:
-    parsed_url = urlparse(youtube_url)
-
-    if parsed_url.hostname in ("www.youtube.com", "youtube.com"):
-        return parse_qs(parsed_url.query).get("v", [None])[0]
-
-    if parsed_url.hostname == "youtu.be":
-        return parsed_url.path.lstrip("/")
-
-    raise ValueError("Invalid YouTube URL")
+ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
 
 
-def fetch_transcript(youtube_url: str):
-    video_id = get_video_id(youtube_url)
+def fetch_playlist_transcripts(playlist_url: str, output_dir="transcripts"):
+    playlist = Playlist(playlist_url)
 
-    ytt_api = YouTubeTranscriptApi()
-    transcript_list = ytt_api.fetch(video_id)
+    os.makedirs(output_dir, exist_ok=True)
 
-    output_file = f"{video_id}.txt"
+    print(f"Found {len(playlist.video_urls)} videos in playlist")
 
-    with open(output_file, "w", encoding="utf-8") as f:
-        for snippet in transcript_list:
-            f.write(snippet.text + " ")
+    for idx, video_url in enumerate(playlist.video_urls, start=1):
+        video_id = video_url.split("v=")[-1]
+        time.sleep(1)
 
-    print(f"Transcript saved to {output_file}")
+        try:
+            proxy_username = os.environ.get("PROXY_USERNAME")
+            proxy_password = os.environ.get("PROXY_PASSWORD")
+            
+            ytt_api = YouTubeTranscriptApi(
+                proxy_config=WebshareProxyConfig(
+                    proxy_username=proxy_username,
+                    proxy_password=proxy_password,
+                )
+            )
+            transcript_list = ytt_api.fetch(video_id)
+
+            output_file = os.path.join(output_dir, f"{video_id}.txt")
+            with open(output_file, "w", encoding="utf-8") as f:
+                for snippet in transcript_list:
+                    # print(snippet.text)
+                    f.write(snippet.text + " ")
+
+            print(f"[{idx}] Saved transcript → {output_file}")
+
+        except TranscriptsDisabled:
+            print(f"[{idx}] Transcripts disabled for video: {video_id}")
+        except NoTranscriptFound:
+            print(f"[{idx}] No transcript found for video: {video_id}")
+        except Exception as e:
+            print(f"[{idx}] Failed for {video_id}: {e}")
 
 
 if __name__ == "__main__":
-    url = "https://youtu.be/zg_66Q3oSss"
-    fetch_transcript(url)
+    playlist_url = "https://youtube.com/playlist?list=PLMCXHnjXnTnvo6alSjVkgxV-VH6EPyvoX"
+    fetch_playlist_transcripts(playlist_url)
